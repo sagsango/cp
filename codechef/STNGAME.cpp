5c4b409782e709e1535098d2d7922d4f4c236044
sagsango <sagar.it.iiests@gmail.com>
2022-02-18 11:46:23 +0530

    ++Kickstart, ++Codechef

---
+++ codechef/STNGAME.cpp
@@ -0,0 +1,184 @@
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <vector>
+#include <set>
+#include <map>
+#include <cstring>
+#include <string>
+#include <cmath>
+#include <cassert>
+#include <ctime>
+#include <algorithm>
+#include <numeric>
+#include <sstream>
+#include <list>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <cstdlib>
+#include <cstdio>
+#include <iterator>
+#include <functional>
+#include <bitset>
+#include <unordered_map>
+#include <unordered_set>
+
+#ifdef LOCAL
+#define WHILE_ONCE  while(false)
+#define RESET           "\033[0m"
+#define RED             "\033[31m"        /* Red */
+#define GREEN           "\033[32m"        /* Green */
+#define YELLOW          "\033[33m"        /* Yellow */
+#define BLUE            "\033[34m"        /* Blue */
+#define MAGENTA         "\033[35m"        /* Magenta */
+#define CYAN            "\033[36m"        /* Cyan */
+#define WHITE           "\033[37m"        /* White */
+#define BOLDBLACK   "\033[1m\033[30m"     /* Bold Black */
+#define BOLDRED     "\033[1m\033[31m"     /* Bold Red */
+#define BOLDGREEN   "\033[1m\033[32m"     /* Bold Green */
+#define BOLDYELLOW  "\033[1m\033[33m"     /* Bold Yellow */
+#define BOLDBLUE    "\033[1m\033[34m"     /* Bold Blue */
+#define BOLDMAGENTA "\033[1m\033[35m"     /* Bold Magenta */
+#define BOLDCYAN    "\033[1m\033[36m"     /* Bold Cyan */
+#define BOLDWHITE   "\033[1m\033[37m"     /* Bold White */
+#define cerr cerr<<RED
+#define endl RESET<<endl
+#define bug1( x )                   do{ cerr << (#x) <<"="<< x << endl; }WHILE_ONCE;
+#define bug2( x , y )               do{ cerr << (#x) <<"="<< x << "    " << (#y) <<"="<< (y) << endl; }WHILE_ONCE;
+#define bug3( x , y , z )           do{ cerr << (#x) <<"="<< x << "    " << (#y) <<"="<< (y) << "    " << (#z) <<"="<< (z) << endl; }WHILE_ONCE;
+#define bug4( x , y , z , w)        do{ cerr << (#x) <<"="<< x << "    " << (#y) <<"="<< (y) << "    " << (#z) <<"="<< (z) << "    " << (#w) <<"="<< w << endl; }WHILE_ONCE;
+#define bug5( x , y , z , w ,p)     do{ cerr << (#x) <<"="<< x << "    " << (#y) <<"="<< (y) << "    " << (#z) <<"="<< (z) << "    " << (#w) <<"="<< w << "    " << (#p) <<"="<< p << endl; }WHILE_ONCE;
+#define bug6( x , y , z , w ,p , q) do{ cerr << (#x) <<"="<< x << "    " << (#y) <<"="<< (y) << "    " << (#z) <<"="<< (z) << "    " << (#w) <<"="<< w << "    " << (#p) <<"="<< p << "    " << (#q) <<"="<< q << endl; }WHILE_ONCE;
+#define bugn( x , n )               do{ cerr << (#x) << endl; for(int i=0;i<n;i++){ cout << x[i] << "    "; } cout << endl; }WHILE_ONCE;
+#define bugnm( x , n , m )          do{ cerr << (#x) << endl; for(int i=0;i<n;i++){ for(int j=0;j<m;j++) cout << x[i][j] << "    "; } cout << endl; }WHILE_ONCE;
+#else
+#define bug1( x )
+#define bug2( x , y )
+#define bug3( x , y , z )
+#define bug4( x , y , z , w )
+#define bug5( x , y , z , w ,p )
+#define bug6( x , y , z , w ,p , q )
+#define bugn( x , n )
+#define bugnm( x , n , m )
+#endif // LOCAL
+
+typedef unsigned long long ul;
+typedef long double ld;
+typedef long long ll;
+using namespace std;
+
+template<typename T, typename K>
+inline bool smax(T &x,K y){ return x < y ? x = y, true : false; }
+template<typename T, typename K>
+inline bool smin(T &x,K y){ return x > y ? x = y, true : false; }
+
+
+
+const int mod = 1e9+7;
+int add(int x,int y){
+    int z = x + y;
+    if( z >= mod ){
+        z -= mod;
+    }
+    return z;
+}
+int sub(int x,int y){
+    int z = x - y;
+    if( z < 0 ){
+        z += mod;
+    }
+    return z;
+}
+int mul(int x,int y){
+    ll z = 1ll * x * y;
+    if( z >= mod ){
+        z %= mod;
+    }
+    return z;
+}
+int binpow(int a,ll p){
+    int r = 1;
+    while( p ){
+        if( p & 1 )
+            r = mul(r,a);
+        a = mul(a,a), p >>= 1;
+    }
+    return r;
+}
+
+
+int main(){
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    int T; cin >> T;
+    for(int tt=1;tt<=T;++tt){
+			int n; cin >> n;
+
+			vector<int>alice(26), bob(26);
+			string s, ans(2*n,'\0');
+			cin >> s;
+			for(auto &c:s){
+				alice[c-'a'] += 1;
+			}
+			cin >> s;
+			for(auto &c:s){
+				bob[c-'a'] += 1;
+			}
+			int l = 0, r = 2*n-1, p = 0, ALICE = 0, BOB = 1, ai, bi, k;
+			while( l <= r ){
+
+				if( (p&1) == ALICE ){
+					ai = 0;
+					while( alice[ai] == 0 )
+						++ai;
+					int should_fill = 0, cnt = 0;
+					for(int i=ai+1;i<26;++i){
+						if( bob[i] ){
+							should_fill = 1;
+						}
+					}
+					if( should_fill ){
+						ans[l] = ai + 'a';
+						--alice[ai];
+						++l;
+					}else{
+						ai = 25;
+						while( alice[ai] == 0 )
+							--ai;
+						ans[r] = ai + 'a';
+						--alice[ai];
+						--r;
+					}
+				}
+
+				if( (p&1) == BOB ){
+					bi = 25;
+					while( bob[bi] == 0 )
+						--bi;
+					int should_fill = 0, cnt = 0;
+					for(int i=0;i<bi;++i){
+						if( alice[i] ){
+							should_fill = 1;
+						}
+					}
+					if( should_fill ){
+						ans[l] = bi + 'a';
+						--bob[bi];
+						l++;
+					}else{
+						bi = 0;
+						while( bob[bi] == 0 )
+							++bi;
+						ans[r] = bi + 'a';
+						--bob[bi];
+						--r;
+					}
+				}
+
+				p += 1;
+			}
+
+			cout << ans << endl;
+
+    }
+}
