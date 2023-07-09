e1be293bd43d0724994170c90748463c8cbff1ad
sagsango <sagar.it.iiests@gmail.com>
2023-07-10 02:10:26 +0530

    10 July, After Getting Approved my F1 Visa.

---
+++ codeforces/round/0881_div3/notes.c
@@ -0,0 +1,1 @@
+Last 2 Problmes are very Interesting.
---
+++ codeforces/round/0883_div3/c.cpp
@@ -0,0 +1,167 @@
+/*
+#pragma GCC target("popcnt")
+#pragma comment(linker, "/stack:200000000")
+#pragma GCC optimize("Ofast")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+*/
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <vector>
+#include <set>
+#include <map>
+#include <cstring>
+#include <string>
+#include <cmath>
+#include <cassert>
+#include <ctime>
+#include <algorithm>
+#include <numeric>
+#include <sstream>
+#include <list>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <cstdlib>
+#include <cstdio>
+#include <iterator>
+#include <functional>
+#include <bitset>
+#include <unordered_map>
+#include <unordered_set>
+
+#ifdef LOCAL
+#define WHILE_ONCE  while(false)
+#define RESET           "\033[0m"
+#define RED             "\033[31m"        /* Red */
+#define GREEN           "\033[32m"        /* Green */
+#define YELLOW          "\033[33m"        /* Yellow */
+#define BLUE            "\033[34m"        /* Blue */
+#define MAGENTA         "\033[35m"        /* Magenta */
+#define CYAN            "\033[36m"        /* Cyan */
+#define WHITE           "\033[37m"        /* White */
+#define BOLDBLACK   "\033[1m\033[30m"     /* Bold Black */
+#define BOLDRED     "\033[1m\033[31m"     /* Bold Red */
+#define BOLDGREEN   "\033[1m\033[32m"     /* Bold Green */
+#define BOLDYELLOW  "\033[1m\033[33m"     /* Bold Yellow */
+#define BOLDBLUE    "\033[1m\033[34m"     /* Bold Blue */
+#define BOLDMAGENTA "\033[1m\033[35m"     /* Bold Magenta */
+#define BOLDCYAN    "\033[1m\033[36m"     /* Bold Cyan */
+#define BOLDWHITE   "\033[1m\033[37m"     /* Bold White */
+#define cerr cerr<<RED
+#define endl RESET<<endl
+#define bug1( x )                   do{ cerr << (#x) <<"="<< x << endl; }WHILE_ONCE;
+#define bug2( x , y )               do{ cerr << (#x) <<"="<< x << "    " << (#y) <<"="<< (y) << endl; }WHILE_ONCE;
+#define bug3( x , y , z )           do{ cerr << (#x) <<"="<< x << "    " << (#y) <<"="<< (y) << "    " << (#z) <<"="<< (z) << endl; }WHILE_ONCE;
+#define bug4( x , y , z , w)        do{ cerr << (#x) <<"="<< x << "    " << (#y) <<"="<< (y) << "    " << (#z) <<"="<< (z) << "    " << (#w) <<"="<< w << endl; }WHILE_ONCE;
+#define bug5( x , y , z , w ,p)     do{ cerr << (#x) <<"="<< x << "    " << (#y) <<"="<< (y) << "    " << (#z) <<"="<< (z) << "    " << (#w) <<"="<< w << "    " << (#p) <<"="<< p << endl; }WHILE_ONCE;
+#define bug6( x , y , z , w ,p , q) do{ cerr << (#x) <<"="<< x << "    " << (#y) <<"="<< (y) << "    " << (#z) <<"="<< (z) << "    " << (#w) <<"="<< w << "    " << (#p) <<"="<< p << "    " << (#q) <<"="<< q << endl; }WHILE_ONCE;
+#define bugn( x , n )               do{ cerr << (#x) << endl; for(int i=0;i<n;i++){ cout << x[i] << "    "; } cout << endl; }WHILE_ONCE;
+#define bugnm( x , n , m )          do{ cerr << (#x) << endl; for(int i=0;i<n;i++){ for(int j=0;j<m;j++) cout << x[i][j] << "    "; } cout << endl; }WHILE_ONCE;
+#else
+#define bug1( x )
+#define bug2( x , y )
+#define bug3( x , y , z )
+#define bug4( x , y , z , w )
+#define bug5( x , y , z , w ,p )
+#define bug6( x , y , z , w ,p , q )
+#define bugn( x , n )
+#define bugnm( x , n , m )
+#endif // LOCAL
+
+typedef unsigned long long ul;
+typedef long double ld;
+typedef long long ll;
+using namespace std;
+
+template<typename T, typename K>
+inline bool smax(T &x,K y){ return x < y ? x = y, true : false; }
+template<typename T, typename K>
+inline bool smin(T &x,K y){ return x > y ? x = y, true : false; }
+
+const int mod = 1e9+7;
+int add(int x,int y){
+    int z = x + y;
+    if( z >= mod ){
+        z -= mod;
+    }
+    return z;
+}
+int sub(int x,int y){
+    int z = x - y;
+    if( z < 0 ){
+        z += mod;
+    }
+    return z;
+}
+int mul(int x,int y){
+    ll z = 1ll * x * y;
+    if( z >= mod ){
+        z %= mod;
+    }
+    return z;
+}
+int binpow(int a,ll p){
+    int r = 1;
+    while( p ){
+        if( p & 1 )
+            r = mul(r,a);
+        a = mul(a,a), p >>= 1;
+    }
+    return r;
+}
+
+#define int long long
+struct node{
+	int score, penalty, index;
+	bool operator<(const node & other) const {
+		if (score != other.score)
+			return score > other.score;
+		return penalty < other.penalty;
+	}
+};
+
+int t, n, m, h, penalty, score, t_spend;
+signed main(){
+	ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+	cin >> t;
+	while (t--) {
+		cin >> n >> m >> h;
+		vector<vector<int>> arr(n, vector<int>(m));
+		vector<node> rank;		
+		for (int i=0; i<n; ++i) {
+			for (int j=0; j<m; ++j) {
+				cin >> arr[i][j];
+			}
+			sort(arr[i].begin(), arr[i].end());
+			penalty = score = t_spend = 0;
+			for (int j=0; j<m; ++j) {
+				t_spend += arr[i][j];
+				if (t_spend > h) {
+					break;
+				}
+				score += 1;
+				penalty += t_spend;
+			}
+			rank.push_back({score, penalty, i});
+		}
+		/*
+		sort(rank.begin(), rank.end());
+		for (int i=0; i<n; ++i) {
+			if (rank[i].index == 0) {
+				cout << (i+1) << endl;
+				break;
+			}
+		}
+		*/
+		int better_player = 0;
+		for (int i=1; i<n; ++i) {
+			bug5(rank[0].score, rank[0].penalty, rank[i].score, rank[i].penalty, (rank[0] < rank[i]));
+			if (rank[i] < rank[0]) {
+				better_player++;
+			}
+		}
+		cout << better_player + 1 << endl;
+	}
+}
