c6bc5313a04152d554c7cc57ca41a77ba218e1db
Sagar Singh <sagar.it.iiests@gmail.com>
2025-01-04 18:35:51 -0600

    Div3-codeforces

---
+++ code/codeforces/round/div3/0995_div3/d.cpp
@@ -0,0 +1,203 @@
+/*
+#pragma GCC target("popcnt")
+#pragma comment(linker, "/stack:200000000")
+#pragma GCC optimize("Ofast")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+*/
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <vector>
+#include <set>
+#include <map>
+#include <cstring>
+#include <string>
+#include <cmath>
+#include <cassert>
+#include <ctime>
+#include <algorithm>
+#include <numeric>
+#include <sstream>
+#include <list>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <cstdlib>
+#include <cstdio>
+#include <iterator>
+#include <functional>
+#include <bitset>
+#include <unordered_map>
+#include <unordered_set>
+
+#ifdef LOCAL
+#define WHILE_ONCE  while(false)
+#define RESET           "\033[0m"
+#define RED             "\033[31m"        /* Red */
+#define GREEN           "\033[32m"        /* Green */
+#define YELLOW          "\033[33m"        /* Yellow */
+#define BLUE            "\033[34m"        /* Blue */
+#define MAGENTA         "\033[35m"        /* Magenta */
+#define CYAN            "\033[36m"        /* Cyan */
+#define WHITE           "\033[37m"        /* White */
+#define BOLDBLACK   "\033[1m\033[30m"     /* Bold Black */
+#define BOLDRED     "\033[1m\033[31m"     /* Bold Red */
+#define BOLDGREEN   "\033[1m\033[32m"     /* Bold Green */
+#define BOLDYELLOW  "\033[1m\033[33m"     /* Bold Yellow */
+#define BOLDBLUE    "\033[1m\033[34m"     /* Bold Blue */
+#define BOLDMAGENTA "\033[1m\033[35m"     /* Bold Magenta */
+#define BOLDCYAN    "\033[1m\033[36m"     /* Bold Cyan */
+#define BOLDWHITE   "\033[1m\033[37m"     /* Bold White */
+#define cerr cerr<<RED
+#define endl RESET<<endl
+#define bug1( x )                   do{ cerr << (#x) <<"="<< x << endl; }WHILE_ONCE;
+#define bug2( x , y )               do{ cerr << (#x) <<"="<< x << "    " << (#y) <<"="<< (y) << endl; }WHILE_ONCE;
+#define bug3( x , y , z )           do{ cerr << (#x) <<"="<< x << "    " << (#y) <<"="<< (y) << "    " << (#z) <<"="<< (z) << endl; }WHILE_ONCE;
+#define bug4( x , y , z , w)        do{ cerr << (#x) <<"="<< x << "    " << (#y) <<"="<< (y) << "    " << (#z) <<"="<< (z) << "    " << (#w) <<"="<< w << endl; }WHILE_ONCE;
+#define bug5( x , y , z , w ,p)     do{ cerr << (#x) <<"="<< x << "    " << (#y) <<"="<< (y) << "    " << (#z) <<"="<< (z) << "    " << (#w) <<"="<< w << "    " << (#p) <<"="<< p << endl; }WHILE_ONCE;
+#define bug6( x , y , z , w ,p , q) do{ cerr << (#x) <<"="<< x << "    " << (#y) <<"="<< (y) << "    " << (#z) <<"="<< (z) << "    " << (#w) <<"="<< w << "    " << (#p) <<"="<< p << "    " << (#q) <<"="<< q << endl; }WHILE_ONCE;
+#define bugn( x , n )               do{ cerr << (#x) << endl; for(int i=0;i<n;i++){ cout << x[i] << "    "; } cout << endl; }WHILE_ONCE;
+#define bugnm( x , n , m )          do{ cerr << (#x) << endl; for(int i=0;i<n;i++){ for(int j=0;j<m;j++) cout << x[i][j] << "    "; } cout << endl; }WHILE_ONCE;
+#else
+#define bug1( x )
+#define bug2( x , y )
+#define bug3( x , y , z )
+#define bug4( x , y , z , w )
+#define bug5( x , y , z , w ,p )
+#define bug6( x , y , z , w ,p , q )
+#define bugn( x , n )
+#define bugnm( x , n , m )
+#endif // LOCAL
+
+typedef unsigned long long ul;
+typedef long double ld;
+typedef long long ll;
+using namespace std;
+
+template<typename T, typename K>
+inline bool smax(T &x,K y){ return x < y ? x = y, true : false; }
+template<typename T, typename K>
+inline bool smin(T &x,K y){ return x > y ? x = y, true : false; }
+
+
+
+const int mod = 1e9+7;
+int add(int x,int y){
+    int z = x + y;
+    if( z >= mod ){
+        z -= mod;
+    }
+    return z;
+}
+int sub(int x,int y){
+    int z = x - y;
+    if( z < 0 ){
+        z += mod;
+    }
+    return z;
+}
+int mul(int x,int y){
+    ll z = 1ll * x * y;
+    if( z >= mod ){
+        z %= mod;
+    }
+    return z;
+}
+int binpow(int a,ll p){
+    int r = 1;
+    while( p ){
+        if( p & 1 )
+            r = mul(r,a);
+        a = mul(a,a), p >>= 1;
+    }
+    return r;
+}
+
+
+
+const int nax = 1e6;
+int bit[nax], m;
+
+void update (int i) {
+    while (i < m) {
+        bit[i] += 1;
+        i += i & -i;
+    }
+}
+
+int query (int i) {
+    int sum = 0;
+    while (i > 0) {
+        sum += bit[i];
+        i -= i & -i;
+    }
+    return sum;
+}
+
+int main(){
+	ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+
+
+    /*
+        [ x, x, x, x, x,x ,x ,x ,x ,x x, x, x, x, x, x,x ]
+
+        a, b
+
+        sum - b
+
+        sum - b - a >= low
+        a <= sum - b - low
+
+
+        sum - b - a <= high
+        a >= sum - b - high
+
+    */
+
+
+    int t; cin >> t;
+    while (t--) {
+        ll n, low, high; cin >> n >> low >> high;
+        ll a[n], sum = 0;
+        for (int i=0; i<n; ++i) {
+            cin >> a[i];
+            sum += a[i];
+        }
+
+        vector <ll> order;
+        for (int i=0; i<n; ++i) {
+            order.push_back(a[i]);
+            order.push_back(sum - a[i] - low);
+            order.push_back(sum - a[i] - high - 1);
+        }
+
+        sort (order.begin(), order.end());
+        order.erase(unique(order.begin(), order.end()), order.end());
+
+
+        #define idx(x) (lower_bound(order.begin(), order.end(), x) - order.begin() + 1)
+
+        m = order.size() + 10;
+        
+        ll pairs = 0;
+        for (int i=0; i<n; ++i) {
+            ll mx = sum - a[i] - low;
+            ll mn = sum - a[i] - high;
+            if (mn <= mx) {
+                pairs += query(idx(mx)) - query(idx(mn-1));
+                //cout << i+1 <<" " << query(idx(mx)) - query(idx(mn-1)) << endl;
+                //bug5(i+1, mx, mn, query(idx(mx)), query(idx(mn-1)));
+            }
+            //bug3("update", a[i], idx(a[i]));
+            update (idx(a[i]));
+        }
+        for (int i=0; i<m; ++i) {
+            bit[i] = 0;
+        }
+
+        cout << pairs << endl;
+    }
+	
+}
+
